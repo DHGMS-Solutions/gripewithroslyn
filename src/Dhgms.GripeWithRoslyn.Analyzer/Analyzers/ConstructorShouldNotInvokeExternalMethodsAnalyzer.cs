fa8f0cb7f0b2d2aa00ba7ab47309bb0df0751f98
David Vreony <dpvreony@users.noreply.github.com>
2018-11-09 19:18:04 +0000

    placeholder for ctor analyzer

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/ConstructorShouldNotInvokeExternalMethodsAnalyzer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers
+{
+    public sealed class ConstructorShouldNotInvokeExternalMethodsAnalyzer
+    {
+    }
+}

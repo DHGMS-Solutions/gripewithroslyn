3663f6b50c038c5b250f504515751c44335f2894
David Vreony <dpvreony@users.noreply.github.com>
2019-04-17 17:19:37 +0100

    initial work for System.Type.GetType

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer.UnitTests/Analyzers/UseTypeofInsteadOfTypeGetTypeAnalyzerTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dhgms.GripeWithRoslyn.Analyzer.Analyzers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using TestHelper;
+using Xunit;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.UnitTests.Analyzers
+{
+    public sealed class UseTypeofInsteadOfTypeGetTypeAnalyzerTest : CodeFixVerifier
+    {
+        //Diagnostic and CodeFix both triggered and checked for
+        [Fact]
+        public void ReturnsWarning()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        using System.Text;
+
+        class TypeName
+        {
+            public void MethodName()
+            {
+                System.Type.GetType(""System.String"");
+                char[] testValue = null;
+
+                ASCIIEncoding.GetBytes(testValue, 0, 0);
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticIdsHelper.UseEncodingUnicodeInsteadOfASCII,
+                Message = "Consider usage of typeof(x) instead of System.Type.GetType.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                        new DiagnosticResultLocation("Test0.cs", 11, 17)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        //protected override CodeFixProvider GetCSharpCodeFixProvider()
+        //{
+        //    return new DhgmsGripeWithRoslynAnalyzerCodeFixProvider();
+        //}
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new UseTypeofInsteadOfTypeGetTypeAnalyzer();
+        }
+    }
+}
---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/UseTypeofInsteadOfTypeGetTypeAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeCracker;
+using Microsoft.CodeAnalysis;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers
+{
+    public sealed class UseTypeofInsteadOfTypeGetTypeAnalyzer : BaseInvocationUsingClassAnalyzer
+    {
+        private const string Title = "Consider usage of typeof(x) instead of System.Type.GetType.";
+
+        private const string MessageFormat = Title;
+
+        private const string Category = SupportedCategories.Maintainability;
+
+        private const string Description =
+            "System.Type.GetType may be a misuse of establishling type information. typeof(x) allows for compile time verification of the type.";
+
+        public UseTypeofInsteadOfTypeGetTypeAnalyzer() : base(
+            DiagnosticIdsHelper.UseEncodingUnicodeInsteadOfASCII,
+            Title,
+            MessageFormat,
+            Category,
+            Description,
+            DiagnosticSeverity.Warning)
+        {
+        }
+
+        protected override string ClassName => "global::System.Type";
+    }
+}

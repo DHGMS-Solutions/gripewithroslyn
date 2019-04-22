fd3bdf341a4ff75e7a01eb0e4ad5d34495b129de
David Vreony <dpvreony@users.noreply.github.com>
2019-04-22 18:01:01 +0100

    #25 passes test

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer.UnitTests/Analyzers/UseTypeofInsteadOfBaseMethodDeclaringTypeAnalyzerTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Dhgms.GripeWithRoslyn.Analyzer.Analyzers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using TestHelper;
+using Xunit;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.UnitTests.Analyzers
+{
+    public sealed class UseTypeofInsteadOfBaseMethodDeclaringTypeAnalyzerTest : CodeFixVerifier
+    {
+        //Diagnostic and CodeFix both triggered and checked for
+        [Fact]
+        public void ReturnsWarning()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        using System.Text;
+
+        class TypeName
+        {
+            public void MethodName()
+            {
+                global::System.Reflection.MethodBase.GetCurrentMethod().DeclaringType;
+            }
+        }
+    }";
+
+            var a = MethodBase.GetCurrentMethod().DeclaringType;
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticIdsHelper.UseEncodingUnicodeInsteadOfASCII,
+                Message = "Consider usage of typeof(x) instead of MethodBase.GetCurrentMethod().DeclaringType.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                        new DiagnosticResultLocation("Test0.cs", 10, 17)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        //protected override CodeFixProvider GetCSharpCodeFixProvider()
+        //{
+        //    return new DhgmsGripeWithRoslynAnalyzerCodeFixProvider();
+        //}
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new UseTypeofInsteadOfBaseMethodDeclaringTypeAnalyzer();
+        }
+    }
+
+}

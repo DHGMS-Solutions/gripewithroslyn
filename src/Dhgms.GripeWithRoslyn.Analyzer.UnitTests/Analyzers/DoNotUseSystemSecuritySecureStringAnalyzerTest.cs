b68efeec401c8fd6a351ffa680f631ce6f48b9b1
David Vreony <dpvreony@users.noreply.github.com>
2019-08-18 14:33:59 +0100

    #60 do not use securestring

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer.UnitTests/Analyzers/DoNotUseSystemSecuritySecureStringAnalyzerTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dhgms.GripeWithRoslyn.Analyzer.Analyzers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using TestHelper;
+using Xunit;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.UnitTests.Analyzers
+{
+    public sealed class DoNotUseSystemSecuritySecureStringAnalyzerTest : CodeFixVerifier
+    {
+        //Diagnostic and CodeFix both triggered and checked for
+        [Fact]
+        public void ReturnsWarning()
+        {
+            var test = @"
+    namespace System.Security
+    {
+        public sealed class SecureString
+        {
+            public void Clear()
+            {
+            }
+        }
+    }
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void MethodName()
+            {
+                var secureString = new System.Security.SecureString();
+                secureString.Clear();
+            }
+        }
+    }";
+            var methodInvoke = new DiagnosticResult
+            {
+                Id = DiagnosticIdsHelper.DoNotUseSystemSecuritySecureString,
+                Message = DoNotUseSystemSecuritySecureStringAnalyzer.Title,
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 19, 17)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(
+                test,
+                methodInvoke);
+        }
+
+        //protected override CodeFixProvider GetCSharpCodeFixProvider()
+        //{
+        //    return new DhgmsGripeWithRoslynAnalyzerCodeFixProvider();
+        //}
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotUseSystemSecuritySecureStringAnalyzer();
+        }
+    }
+}

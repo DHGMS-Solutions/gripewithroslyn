1b788c6fac89f580aa6fcd3fbbbe1d5ff7c009ca
David Vreony <dpvreony@users.noreply.github.com>
2019-10-05 19:42:16 +0100

    #66 suggest system.text.json

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/UseSystemTextJsonInsteadOfNewtonsoftJson.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dhgms.GripeWithRoslyn.Analyzer.CodeCracker.Extensions;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers
+{
+    public sealed class UseSystemTextJsonInsteadOfNewtonsoftJson : BaseInvocationUsingNamespaceAnalyzer
+    {
+        internal const string Title = "Consider use of System.Text.Json instead of Newtonsoft.Json (JSON.NET).";
+
+        private const string MessageFormat = Title;
+
+        private const string Category = SupportedCategories.Design;
+
+        private const string Description =
+            "System.Text.Json brings improvements from JSON.NET.";
+
+        public UseSystemTextJsonInsteadOfNewtonsoftJson() : base(
+            DiagnosticIdsHelper.UseSystemTextJsonInsteadOfNewtonsoftJson,
+            Title,
+            MessageFormat,
+            Category,
+            Description,
+            DiagnosticSeverity.Warning)
+        {
+        }
+
+        protected override string Namespace => "global::Newtonsoft.Json";
+    }
+}
--- src/Dhgms.GripeWithRoslyn.Analyzer/DiagnosticIdsHelper.cs
+++ src/Dhgms.GripeWithRoslyn.Analyzer/DiagnosticIdsHelper.cs
@@ -11,19 +11,33 @@ namespace Dhgms.GripeWithRoslyn.Analyzer
         internal static string FluentDataAutoMapAnalyzer => "GR0000";
         internal static string FluentDataQueryManyWithNullArgumentAnalyzer => "GR0002";
         internal static string OracleUnmanagedDllDataAccessDllAnalyzer => "GR0003";
+        
         internal static string RemotingServicesAnalyzer => "GR0004";
+        
         internal static string ViewModelClassShouldInheritReactiveObject => "GR0005";
+        
         internal static string TreatWarningsAsErrorsShouldBeEnabled => "GR0006";
+        
         internal static string UseEncodingUnicodeInsteadOfASCII => "GR0007";
+        
         internal static string StructureMapShouldNotBeUsed => "GR0008";
+        
         internal static string ReactiveObjectClassShouldHaveViewModelSuffix => "GR0009";
+        
         internal static string ReactiveObjectInterfaceShouldHaveViewModelSuffix => "GR0010";
+        
         internal static string ViewModelInterfaceShouldInheritReactiveObject => "GR0011";
+        
         internal static string ConstructorShouldNotInvokeExternalMethods => "GR0012";
 
         internal static string DoNotUseGdiPlus => "GR0013";
+        
         internal static string UseDateTimeUtcNowInsteadofNow => "GR0014";
+        
         internal static string DoNotUseSystemConsole => "GR0015";
+
         internal static string DoNotUseSystemSecuritySecureString => "GR0016";
+
+        internal static string UseSystemTextJsonInsteadOfNewtonsoftJson => "GR0017";
     }
 }

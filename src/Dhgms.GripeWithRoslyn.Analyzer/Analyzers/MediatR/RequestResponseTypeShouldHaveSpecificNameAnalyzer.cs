09d7b3a5094786c84b8bbf404a749ecf47a53be4
dpvreony <dpvreony@users.noreply.github.com>
2022-08-18 19:34:03 +0100

    stubs for mediatr analyzers

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/MediatR/RequestResponseTypeShouldHaveSpecificNameAnalyzer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers.MediatR
+{
+    internal class RequestResponseTypeShouldHaveSpecificNameAnalyzer
+    {
+    }
+}

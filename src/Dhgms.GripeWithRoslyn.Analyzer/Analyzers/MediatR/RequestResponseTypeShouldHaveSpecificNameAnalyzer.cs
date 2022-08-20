ae42ec166594ddba261019aea43de5009e034ad0
David Vreony <dpvreony@users.noreply.github.com>
2022-08-20 23:17:31 +0100

    prep for validating type arg on command\query

--- src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/MediatR/RequestResponseTypeShouldHaveSpecificNameAnalyzer.cs
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/MediatR/RequestResponseTypeShouldHaveSpecificNameAnalyzer.cs
@@ -2,9 +2,11 @@
 // This file is licensed to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
+using Dhgms.GripeWithRoslyn.Analyzer.Analyzers.Abstractions;
+
 namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers.MediatR
 {
-    internal class RequestResponseTypeShouldHaveSpecificNameAnalyzer
+    public sealed class RequestResponseTypeShouldHaveSpecificNameAnalyzer : AbstractTypeArgumentForClassShouldEndWithSpecificSuffixAnalyzer
     {
     }
 }

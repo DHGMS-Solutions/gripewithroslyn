3fe3cfbf03c1b3f316df1acc2802fcd226df0311
David Vreony <dpvreony@users.noreply.github.com>
2023-09-23 14:48:53 +0100

    placeholder for efcore migration analyzer

--- src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/AspNetCore/ApiShouldUseGenericActionResultAnalyzer.cs
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/AspNetCore/ApiShouldUseGenericActionResultAnalyzer.cs
@@ -2,7 +2,6 @@
 // This file is licensed to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-
 namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers.AspNetCore
 {
     internal class ApiShouldUseGenericActionResultAnalyzer
---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/EfCore/MigrationViabilityAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers.EfCore
+{
+    /// <summary>
+    /// This checks for the minimum requirements for a migration to be viable.
+    /// This is to prevent risks where people can forget to check in the designer file
+    /// or make breaking changes to it, that don't get picked up by the build or eve
+    /// the application of the migration itself, because it can be skipped.
+    /// </summary>
+    public sealed class MigrationViabilityAnalyzer
+    {
+    }
+}

8aed59c4ecc32558a48c816e6ae28fa5d9367f31
David Vreony <dpvreony@users.noreply.github.com>
2023-12-29 20:30:56 +0000

    feature: Abstract class name prefix check (#241)

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/Language/ClassWithAbstractKeywordAnalyzer.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Immutable;
+using Dhgms.GripeWithRoslyn.Analyzer.CodeCracker.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers.Language
+{
+    /// <summary>
+    /// Analyzer to check if a class with the abstract keyword starts with the word "Abstract".
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class ClassWithAbstractKeywordAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string Title = "Class with abstract keyword should start with Abstract";
+        private readonly DiagnosticDescriptor _rule;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClassWithAbstractKeywordAnalyzer"/> class.
+        /// </summary>
+        public ClassWithAbstractKeywordAnalyzer()
+        {
+            _rule = new DiagnosticDescriptor(
+                DiagnosticIdsHelper.ClassWithAbstractKeyword,
+                "Class with abstract keyword should start with Abstract",
+                "Class with abstract keyword should start with Abstract",
+                SupportedCategories.Naming,
+                DiagnosticSeverity.Warning,
+                isEnabledByDefault: true,
+                description: "Class with abstract keyword should start with Abstract");
+        }
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_rule);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.RegisterSyntaxNodeAction(AnalyzeClassDeclarationExpression, SyntaxKind.ClassDeclaration);
+        }
+
+        private void AnalyzeClassDeclarationExpression(SyntaxNodeAnalysisContext context)
+        {
+            if (!(context.Node is ClassDeclarationSyntax classDeclarationSyntax))
+            {
+                return;
+            }
+
+            if (!classDeclarationSyntax.Modifiers.Any(SyntaxKind.AbstractKeyword))
+            {
+                return;
+            }
+
+            var identifier = classDeclarationSyntax.Identifier;
+            if (identifier.Text.StartsWith("Abstract", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(_rule, identifier.GetLocation()));
+        }
+    }
+}
---
+++ src/Dhgms.GripeWithRoslyn.UnitTests/Analyzer/Analyzers/Language/ClassWithAbstractKeywordAnalyzerTests.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Dhgms.GripeWithRoslyn.Analyzer;
+using Dhgms.GripeWithRoslyn.Analyzer.Analyzers.Language;
+using Dhgms.GripeWithRoslyn.UnitTests.Helpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace Dhgms.GripeWithRoslyn.UnitTests.Analyzer.Analyzers.Language
+{
+    /// <summary>
+    /// Unit Tests for <see cref="ClassWithAbstractKeywordAnalyzer"/>.
+    /// </summary>
+    public sealed class ClassWithAbstractKeywordAnalyzerTests : CodeFixVerifier
+    {
+        /// <summary>
+        /// Test to ensure bad code returns a warning.
+        /// </summary>
+        [Fact]
+        public void ReturnsWarning()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        using System.Text;
+
+        public abstract class TypeName
+        {
+        }
+    }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticIdsHelper.ClassWithAbstractKeyword,
+                Message = ClassWithAbstractKeywordAnalyzer.Title,
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 6, 31)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        /// <inheritdoc />
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ClassWithAbstractKeywordAnalyzer();
+        }
+    }
+}

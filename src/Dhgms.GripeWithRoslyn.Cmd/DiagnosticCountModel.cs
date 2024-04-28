9b15e4ebdbceb9084ecc6723d59e1023eeae4f2b
David Vreony <dpvreony@users.noreply.github.com>
2024-04-28 21:24:13 +0100

    bump nuget packages

---
+++ src/Dhgms.GripeWithRoslyn.Cmd/DiagnosticCountModel.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace Dhgms.GripeWithRoslyn.Cmd
+{
+    internal class DiagnosticCountModel
+    {
+    }
+}

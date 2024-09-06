fd191956d4d29b7f7cc92f989a92684030d55bd9
David Vreony <dpvreony@users.noreply.github.com>
2024-09-06 21:15:03 +0100

    initial logic for using a proper test harness

---
+++ src/Dhgms.GripeWithRoslyn.UnitTests/Analyzer/Analyzers/EfCore/QueriesShouldUseProjectionsAnalyzerTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Linq;
+using System.Threading.Tasks;
+using Dhgms.GripeWithRoslyn.Analyzer.Analyzers.EfCore;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.MSBuild;
+using Xunit;
+
+namespace Dhgms.GripeWithRoslyn.UnitTests.Analyzer.Analyzers.EfCore
+{
+    /// <summary>
+    /// Units tests for <see cref="QueriesShouldUseProjectionsAnalyzer"/>.
+    /// </summary>
+    public sealed class QueriesShouldUseProjectionsAnalyzerTests
+    {
+        /// <summary>
+        /// Test to check the analyzer runs.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [Fact]
+        public async Task ValidateAnalyzerReturns()
+        {
+            var visualStudioInstances = MSBuildLocator.QueryVisualStudioInstances().ToArray();
+            MSBuildLocator.RegisterInstance(visualStudioInstances[0]);
+
+            using (var workspace = MSBuildWorkspace.Create())
+            {
+                var project =
+                    await workspace.OpenProjectAsync(
+                        "C:\\GitHub\\dhgms-solutions\\gripewithroslyn\\src\\Gripe.Testing\\Gripe.Testing.csproj");
+
+                var analyzer = GetAnalyzer();
+                var compilation = await project.GetCompilationAsync();
+                var compilationWithAnalyzers = compilation.WithAnalyzers([analyzer]);
+                var diagnostics = await compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync();
+                Assert.NotEmpty(diagnostics);
+            }
+        }
+
+        private DiagnosticAnalyzer GetAnalyzer()
+        {
+            return new QueriesShouldUseProjectionsAnalyzer();
+        }
+    }
+}
---
+++ src/Gripe.Testing/EfCore/GetListWithoutSelection.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gripe.Testing.EfCore
+{
+    /// <summary>
+    /// Test query for running an EFCore query without using a projection.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public sealed class GetListWithoutSelection
+    {
+        private readonly IDbContextFactory<IdentityDbContext> _dbContextFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetListWithoutSelection"/> class.
+        /// </summary>
+        /// <param name="dbContextFactory">Factory for creating DbContext instances.</param>
+        public GetListWithoutSelection(IDbContextFactory<IdentityDbContext> dbContextFactory)
+        {
+            ArgumentNullException.ThrowIfNull(dbContextFactory);
+            _dbContextFactory = dbContextFactory;
+        }
+
+        /// <summary>
+        /// Gets a list of users.
+        /// </summary>
+        /// <returns>List of users.</returns>
+        public async Task<List<IdentityUser>> GetResult()
+        {
+            using (var dbContext = await _dbContextFactory.CreateDbContextAsync().ConfigureAwait(false))
+            {
+                return await dbContext.Users.ToListAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}

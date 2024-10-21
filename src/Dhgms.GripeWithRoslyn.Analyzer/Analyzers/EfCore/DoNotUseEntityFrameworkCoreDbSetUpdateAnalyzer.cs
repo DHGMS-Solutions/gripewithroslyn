56280c943266ded9f381ac48be90482f39340f5e
David Vreony <dpvreony@users.noreply.github.com>
2024-10-21 21:35:34 +0100

    logic to warn about use of DbSet Update

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/EfCore/DoNotUseEntityFrameworkCoreDbSetUpdateAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Dhgms.GripeWithRoslyn.Analyzer.Analyzers.Abstractions;
+using Dhgms.GripeWithRoslyn.Analyzer.CodeCracker.Extensions;
+using Microsoft.CodeAnalysis;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers.EfCore
+{
+    /// <summary>
+    /// Analyzer to ensure the EF Core DbSet method is not used in an Application.
+    /// </summary>
+    [Microsoft.CodeAnalysis.Diagnostics.DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class DoNotUseEntityFrameworkCoreDbSetUpdateAnalyzer : BaseInvocationExpressionAnalyzer
+    {
+        private const string Title = "Do not use Entity Framework DbSet<T>.Update.";
+
+        private const string MessageFormat = Title;
+
+        private const string Category = SupportedCategories.Design;
+
+        private const string Description =
+            "Update should not be used in an Application. It invalidates the entire poco object. Just select the desired entity, update the relevant properties and call SaveChanges.";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoNotUseEntityFrameworkCoreDbSetUpdateAnalyzer"/> class.
+        /// </summary>
+        public DoNotUseEntityFrameworkCoreDbSetUpdateAnalyzer()
+            : base(
+                DiagnosticIdsHelper.DoNotUseEntityFrameworkCoreDbSetUpdate,
+                Title,
+                MessageFormat,
+                Category,
+                Description,
+                DiagnosticSeverity.Warning)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override string MethodName => "Update";
+
+        /// <inheritdoc />
+        protected override string[] ContainingTypes => new[]
+        {
+            "Microsoft.EntityFrameworkCore.DbSet",
+        };
+    }
+}
---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/EfCore/DoNotUseEntityFrameworkCoreDbSetUpdateAsyncAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Dhgms.GripeWithRoslyn.Analyzer.Analyzers.Abstractions;
+using Dhgms.GripeWithRoslyn.Analyzer.CodeCracker.Extensions;
+using Microsoft.CodeAnalysis;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers.EfCore
+{
+    /// <summary>
+    /// Analyzer to ensure the EF Core DbSet method is not used in an Application.
+    /// </summary>
+    [Microsoft.CodeAnalysis.Diagnostics.DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class DoNotUseEntityFrameworkCoreDbSetUpdateAsyncAnalyzer : BaseInvocationExpressionAnalyzer
+    {
+        private const string Title = "Do not use Entity Framework DbSet<T>.UpdateAsync.";
+
+        private const string MessageFormat = Title;
+
+        private const string Category = SupportedCategories.Design;
+
+        private const string Description =
+            "UpdateAsync should not be used in an Application. It invalidates the entire poco object. Just select the desired entity, update the relevant properties and call SaveChangesAsync.";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoNotUseEntityFrameworkCoreDbSetUpdateAsyncAnalyzer"/> class.
+        /// </summary>
+        public DoNotUseEntityFrameworkCoreDbSetUpdateAsyncAnalyzer()
+            : base(
+                DiagnosticIdsHelper.DoNotUseEntityFrameworkCoreDbSetUpdate,
+                Title,
+                MessageFormat,
+                Category,
+                Description,
+                DiagnosticSeverity.Warning)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override string MethodName => "UpdateAsync";
+
+        /// <inheritdoc />
+        protected override string[] ContainingTypes => new[]
+        {
+            "Microsoft.EntityFrameworkCore.DbSet",
+        };
+    }
+}

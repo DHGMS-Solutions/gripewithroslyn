902dd8124b6175fa533e14bb618b2206ad3a3730
David Vreony <dpvreony@users.noreply.github.com>
2025-01-06 19:22:13 +0000

    suggest use of overloads that have IFileProvider

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Analyzers/Runtime/UseFileProviderOverloadAnalyzer.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Dhgms.GripeWithRoslyn.Analyzer.CodeCracker.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Analyzers.Runtime
+{
+    /// <summary>
+    /// Analyzer that checks for methods that have an overload that takes an IFileProvider.
+    /// </summary>
+    [Microsoft.CodeAnalysis.Diagnostics.DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class UseFileProviderOverloadAnalyzer : DiagnosticAnalyzer
+    {
+        private const string Category = SupportedCategories.Design;
+        private static readonly LocalizableString Title = "Method has an overload that takes IFileProvider";
+        private static readonly LocalizableString MessageFormat = "Method '{0}' has an overload that takes IFileProvider";
+        private static readonly LocalizableString Description = "This method has an overload that accepts an IFileProvider, use this to make your code easier to test.";
+
+        private readonly DiagnosticDescriptor _rule;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UseFileProviderOverloadAnalyzer"/> class.
+        /// </summary>
+        public UseFileProviderOverloadAnalyzer()
+        {
+            _rule = new DiagnosticDescriptor(DiagnosticIdsHelper.UseFileProviderOverload, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        }
+
+        /// <inheritdoc />
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_rule);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.RegisterSyntaxNodeAction(
+                AnalyzeInvocationExpression,
+                SyntaxKind.InvocationExpression);
+        }
+
+        private static bool HasFileProviderArgument(IMethodSymbol methodSymbol)
+        {
+            return methodSymbol.Parameters.Any(p => p.Type.ToString() == "Microsoft.Extensions.FileProviders.IFileProvider");
+        }
+
+        private void AnalyzeInvocationExpression(SyntaxNodeAnalysisContext context)
+        {
+            var invocationExpr = (InvocationExpressionSyntax)context.Node;
+
+            if (context.SemanticModel.GetSymbolInfo(invocationExpr).Symbol is not IMethodSymbol methodSymbol)
+            {
+                return;
+            }
+
+            if (HasFileProviderArgument(methodSymbol))
+            {
+                return;
+            }
+
+            var containingType = methodSymbol.ContainingType;
+            var overloads = containingType.GetMembers().OfType<IMethodSymbol>()
+                .Where(m => m.Name == methodSymbol.Name && HasFileProviderArgument(m));
+
+            if (overloads.Any())
+            {
+                var diagnostic = Diagnostic.Create(_rule, invocationExpr.GetLocation(), methodSymbol.Name);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ src/Dhgms.GripeWithRoslyn.Testing/Runtime/Extensions/FileProviderProof.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.Extensions.FileProviders;
+
+namespace Dhgms.GripeWithRoslyn.Testing.Runtime.Extensions
+{
+    /// <summary>
+    /// Analyzer Proofs for <see cref="IFileProvider"/>.
+    /// </summary>
+    public static class FileProviderProof
+    {
+        /// <summary>
+        /// Proof that the analyzer triggers when a <see cref="IFileProvider"/> is not passed as an argument where a relevant overload exists.
+        /// </summary>
+        public static void ProofWarningIsRaised()
+        {
+            SomeMethod();
+        }
+
+        /// <summary>
+        /// Proof that the analyzer doesn't trigger when a <see cref="IFileProvider"/> is passed as an argument, even if another valid overload exists.
+        /// </summary>
+        public static void ProofWarningIsNotRaisedWhereAlreadyUsingFileProviderArg()
+        {
+            var fileProvider = new NullFileProvider();
+            SomeMethod(fileProvider);
+            SomeMethod(fileProvider, 1);
+        }
+
+        private static void SomeMethod()
+        {
+        }
+
+        private static void SomeMethod(IFileProvider fileProvider)
+        {
+            ArgumentNullException.ThrowIfNull(fileProvider);
+        }
+
+        private static void SomeMethod(IFileProvider fileProvider, int i)
+        {
+            ArgumentNullException.ThrowIfNull(fileProvider);
+            ArgumentOutOfRangeException.ThrowIfLessThan(i, 1);
+        }
+    }
+}

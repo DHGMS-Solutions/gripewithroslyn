fb69a3a23833610ad4e6cf42ca6c8fa7c9492c30
David Vreony <dpvreony@users.noreply.github.com>
2025-04-26 23:55:13 +0100

    initial dnt for bin log stats

---
+++ src/Gripe.MSBuildBinLogStats.DotNetTool/CommandLineJob.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.IO.Abstractions;
+using Gripe.MSBuildBinLogStats.DotNetTool.CommandLine;
+using Microsoft.Build.Logging.StructuredLogger;
+using Whipstaff.CommandLine;
+
+namespace Gripe.MSBuildBinLogStats.DotNetTool
+{
+    /// <summary>
+    /// Command line job that handles the parsing of MSBuild bin log files.
+    /// </summary>
+    public sealed class CommandLineJob : AbstractCommandLineHandler<CommandLineArgModel, CommandLineJobLogMessageActionsWrapper>
+    {
+        private readonly IFileSystem _fileSystem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandLineJob"/> class.
+        /// </summary>
+        /// <param name="commandLineJobLogMessageActionsWrapper">Wrapper for logging framework messages.</param>
+        /// <param name="fileSystem">File System abstraction.</param>
+        public CommandLineJob(
+            CommandLineJobLogMessageActionsWrapper commandLineJobLogMessageActionsWrapper,
+            IFileSystem fileSystem)
+            : base(commandLineJobLogMessageActionsWrapper)
+        {
+            ArgumentNullException.ThrowIfNull(fileSystem);
+
+            _fileSystem = fileSystem;
+        }
+
+        /// <inheritdoc/>
+        protected override Task<int> OnHandleCommand(CommandLineArgModel commandLineArgModel)
+        {
+            ArgumentNullException.ThrowIfNull(commandLineArgModel);
+
+            return System.Threading.Tasks.Task.Run(() =>
+            {
+                LogMessageActionsWrapper.StartingHandleCommand();
+
+                var build = BinaryLog.ReadBuild(commandLineArgModel.BinLogPath.FullName);
+
+                var warningCounts = new Dictionary<string, int>();
+                var errorCounts = new Dictionary<string, int>();
+
+                Visit(build, warningCounts, errorCounts);
+
+                Console.WriteLine("Warnings:");
+                foreach (var kvp in warningCounts.OrderBy(kvp => kvp.Key))
+                {
+                    Console.WriteLine($"  {kvp.Key}: {kvp.Value}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Errors:");
+                foreach (var kvp in errorCounts.OrderBy(kvp => kvp.Key))
+                {
+                    Console.WriteLine($"  {kvp.Key}: {kvp.Value}");
+                }
+
+                return 0;
+            });
+        }
+
+        private static void Visit(
+            TreeNode node,
+            IDictionary<string, int> warningCounts,
+            IDictionary<string, int> errorCounts)
+        {
+            if (node is Warning warning && !string.IsNullOrEmpty(warning.Code))
+            {
+                if (warningCounts.TryGetValue(warning.Code, out var count))
+                {
+                    warningCounts[warning.Code] = count + 1;
+                }
+                else
+                {
+                    warningCounts[warning.Code] = 1;
+                }
+            }
+            else if (node is BuildError error && !string.IsNullOrEmpty(error.Code))
+            {
+                if (errorCounts.TryGetValue(error.Code, out var count))
+                {
+                    errorCounts[error.Code] = count + 1;
+                }
+                else
+                {
+                    errorCounts[error.Code] = 1;
+                }
+            }
+
+            foreach (var child in node.Children)
+            {
+                if (child is TreeNode treeNode)
+                {
+                    Visit(treeNode, warningCounts, errorCounts);
+                }
+            }
+        }
+    }
+}
---
+++ src/Gripe.MSBuildBinLogStats.DotNetTool/Program.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using Microsoft.Build.Logging.StructuredLogger;
+using Whipstaff.CommandLine.Hosting;
+
+namespace Gripe.MSBuildBinLogStats.DotNetTool
+{
+    /// <summary>
+    /// Program entry point holder.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        /// <returns>0 for success, 1 for failure.</returns>
+        public static int Main(string[] args)
+        {
+            return HostRunner.RunSimpleCliJob<
+                CommandLineJob,
+                CommandLineArgModel,
+                CommandLineArgModelBinder,
+                CommandLineHandlerFactory>(
+                args,
+                (fileSystem, logger) => new CommandLineJob(
+                    new CommandLineJobLogMessageActionsWrapper(
+                        new CommandLineJobLogMessageActions(),
+                        logger),
+                    fileSystem),
+                new FileSystem());
+        }
+    }
+}

06dde22ff5c6e2d1dfc8d86f004269a6aeae1434
David Vreony <dpvreony@users.noreply.github.com>
2025-04-28 22:54:33 +0100

    Fix: GR0039 properties of object no longer flag if inherited

---
+++ src/Dhgms.GripeWithRoslyn.Analyzer/Extensions/SymbolExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.CodeAnalysis;
+
+namespace Dhgms.GripeWithRoslyn.Analyzer.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="ISymbol"/>.
+    /// </summary>
+    public static class SymbolExtensions
+    {
+        /// <summary>
+        /// Checks if a symbol implements an interface.
+        /// </summary>
+        /// <param name="symbol">Symbol to check.</param>
+        /// <returns>Whether the symbol implements an interface.</returns>
+        public static bool ImplementsInterface(this ISymbol symbol)
+        {
+            // Check if it implements interface methods
+            var implementedInterfaces = symbol.ContainingType.AllInterfaces;
+
+            foreach (var implemented in implementedInterfaces)
+            {
+                foreach (var ifaceMember in implemented.GetMembers())
+                {
+                    var impl = symbol.ContainingType.FindImplementationForInterfaceMember(ifaceMember);
+                    if (SymbolEqualityComparer.Default.Equals(impl, symbol))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
